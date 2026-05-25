67af5013c08ca4687382f1b7d3a4176c6dc7500e
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-05-26 00:05:31 +0200

    fix: pause help usage

--- src/cogs/audio_cog.py
+++ src/cogs/audio_cog.py
@@ -680,7 +680,7 @@ class AudioCog(commands.Cog):
         logger.info(msg.LOG_MULTIPLAY_EXECUTED.format(number_of_tracks=queued_count))
 
 
-    @commands.command(name="pause", help=msg.HELP_MESSAGES["pause"], usage=msg.HELP_MESSAGES["pause"])
+    @commands.command(name="pause", help=msg.HELP_MESSAGES["pause"], usage=msg.HELP_USAGES["pause"])
     async def pause(self, ctx):
         if await self.reject_wrong_text_channel(ctx):
             return
